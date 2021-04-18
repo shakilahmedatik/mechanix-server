2427a15a32880e1682374767d3c89d30c0c60324
Shakil Ahmed Atik <shakilatik@gmail.com>
2021-04-19 04:42:06 +0600

    Final Commit

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ require('dotenv').config()
 
 //Initialize App
 const app = express()
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
+  next()
+})
 
 //Middlewares
 app.use(bodyParser.urlencoded({ extended: false }))
